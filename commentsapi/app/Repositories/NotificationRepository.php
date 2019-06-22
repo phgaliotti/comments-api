34f41208600a9f3f31f5e77e0fc0e20f236e107a
Phelipe Galiotti <phelipe.galiotti@outlook.com.br>
2019-06-22 19:59:18 -0300

    alteracao na consulta de notificacoes por usuario para considerar apenas a data de expiracao

--- commentsapi/app/Repositories/NotificationRepository.php
+++ commentsapi/app/Repositories/NotificationRepository.php
@@ -21,16 +21,14 @@ class NotificationRepository
      }
 
      public function getNotificationsByUserId($id, $pageSize) {
-      //$serverHour = (new DateTime())->modify('-60 minute')->format('Y-m-d');
-      // pegar todos os comentários não lidos ou que a data de expiração seja menor ou igual que a data de (hoje - 60 minutos)
+      // pegar todos as notificacoes não lidos ou que a data de expiração seja menor ou igual que a data de (hoje - 60 minutos)
       $serverHour = Carbon::now()->subMinute(60);
       $responseTeste =  $this->notification
          ->where('to_user_id', '=' , $id)
-         ->Where('read', '=' , false)
-         ->orWhere('expiration_date', '<=' , $serverHour)
+         ->Where('expiration_date', null)
+         ->orWhere('expiration_date', '>=' , $serverHour)
          ->orderBy('created_at', 'desc')
          ->paginate($pageSize);
-         dd($responseTeste);
-         return responseTeste;
+         return $responseTeste;
      }
 }
