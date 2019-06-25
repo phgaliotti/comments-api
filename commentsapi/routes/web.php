36dca2d22499f6363d1921d81910253bbcbb93a1
Phelipe Galiotti <phelipe.galiotti@outlook.com.br>
2019-06-25 00:54:23 -0300

    brincando com rota no get

--- commentsapi/routes/web.php
+++ commentsapi/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "Bem vindo a API de comentários do Phelipe Galiotti! </br>" . $router->app->version();
 });
 
 
